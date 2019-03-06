42c1bffe80e34dd03e5a0bcaf430ff8d01293dcf
Ian Gillingham <ian.gillingham@diamond.ac.uk>
2019-03-06 18:52:26 +0000

    Added hello.html and modified .gitignore

--- restserv.py
+++ restserv.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from flask import request
+from flask import render_template
+from socket import gethostname
+
+app = Flask(__name__)
+
 
 @app.route('/login', methods=['POST', 'GET'])
+@app.route('/hello/')
+@app.route('/hello/<name>')
+
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 def login():
     error = None
     if request.method == 'POST':
@@ -14,3 +25,8 @@ def login():
     # was GET or the credentials were invalid
     return render_template('login.html', error=error)
 
+
+if __name__ == '__main__':
+    # db.create_all()
+    if 'liveconsole' not in gethostname():  # Avoid app.run() if deploying on PythonAnywhere service
+        app.run(host='127.0.0.1',port=8000,debug=True)
